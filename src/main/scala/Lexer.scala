4b8acffa469d3c9a50a241146a93f273a0cdac38
Michalis Pardalos <mpardalos@gmail.com>
2018-01-01 23:14:21 +0200

    Eliminate TokenType

--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -3,155 +3,137 @@ package Raza
 import scala.util.matching.Regex
 import scala.collection.mutable
 
-sealed abstract class TokenType
-object TokenType {
-  case object EOF extends TokenType
-  case object SEMICOLON extends TokenType
+sealed abstract class Token(line: Int, column: Int) 
+object Token {
+  case class EOF(l: Int, c: Int) extends Token(l, c)
+  case class Semicolon(l: Int, c: Int) extends Token(l, c)
 
   // 1-character
-  case object LEFT_BRACKET extends TokenType
-  case object RIGHT_BRACKET extends TokenType
-  case object LEFT_PARENS extends TokenType
-  case object RIGHT_PARENS extends TokenType
-  case object LEFT_BRACE extends TokenType
-  case object RIGHT_BRACE extends TokenType
-  case object COMMA extends TokenType
-  case object COLON extends TokenType
-  case object EQUALS extends TokenType
-  case object PLUS extends TokenType
-  case object MINUS extends TokenType
-  case object SLASH extends TokenType
-  case object STAR extends TokenType
+  case class LeftBracket(l: Int, c: Int) extends Token(l, c)
+  case class RightBracket(l: Int, c: Int) extends Token(l, c)
+  case class LeftParens(l: Int, c: Int) extends Token(l, c)
+  case class RightParens(l: Int, c: Int) extends Token(l, c)
+  case class LeftBrace(l: Int, c: Int) extends Token(l, c)
+  case class RightBrace(l: Int, c: Int) extends Token(l, c)
+  case class Comma(l: Int, c: Int) extends Token(l, c)
+  case class Colon(l: Int, c: Int) extends Token(l, c)
+  case class Equals(l: Int, c: Int) extends Token(l, c)
+  case class Plus(l: Int, c: Int) extends Token(l, c)
+  case class Minus(l: Int, c: Int) extends Token(l, c)
+  case class Slash(l: Int, c: Int) extends Token(l, c)
+  case class Star(l: Int, c: Int) extends Token(l, c)
 
   // Symbols
-  case object FAT_ARROW extends TokenType
-  case object BANG extends TokenType
-  case object BANG_EQUALS extends TokenType
-  case object EQUALS_EQUALS extends TokenType
-  case object GREATER extends TokenType
-  case object LESS extends TokenType
-  case object GREATER_EQUAL extends TokenType
-  case object LESS_EQUAL extends TokenType
+  case class FatArrow(l: Int, c: Int) extends Token(l, c)
+  case class Bang(l: Int, c: Int) extends Token(l, c)
+  case class BangEquals(l: Int, c: Int) extends Token(l, c)
+  case class EqualsEquals(l: Int, c: Int) extends Token(l, c)
+  case class Greater(l: Int, c: Int) extends Token(l, c)
+  case class Less(l: Int, c: Int) extends Token(l, c)
+  case class GreaterEqual(l: Int, c: Int) extends Token(l, c)
+  case class LessEqual(l: Int, c: Int) extends Token(l, c)
 
   // Keywords
-  case object VAR extends TokenType
-  case object LET extends TokenType
-  case object OR extends TokenType
-  case object AND extends TokenType
-  case object IF extends TokenType
-  case object ELSE extends TokenType
-  case object WHILE extends TokenType
-  case object NIL extends TokenType
-  case object RETURN extends TokenType
-  case object TRUE extends TokenType
-  case object FALSE extends TokenType
+  case class Var(l: Int, c: Int) extends Token(l, c)
+  case class Let(l: Int, c: Int) extends Token(l, c)
+  case class Or(l: Int, c: Int) extends Token(l, c)
+  case class And(l: Int, c: Int) extends Token(l, c)
+  case class If(l: Int, c: Int) extends Token(l, c)
+  case class Else(l: Int, c: Int) extends Token(l, c)
+  case class While(l: Int, c: Int) extends Token(l, c)
+  case class Nil(l: Int, c: Int) extends Token(l, c)
+  case class Return(l: Int, c: Int) extends Token(l, c)
+  case class True(l: Int, c: Int) extends Token(l, c)
+  case class False(l: Int, c: Int) extends Token(l, c)
 
   // Literals
-  case object IDENTIFIER extends TokenType
-  case object STRING extends TokenType
-  case object NUMBER extends TokenType
+  case class Identifier(value: String, l: Int, c: Int) extends Token(l, c)
+  case class Str(value: String, l: Int, c: Int) extends Token(l, c)
+  case class Number(value: String, line: Int, column: Int) extends Token(l, c)
 
   // Ignored Tokens
-  case object IGNORE extends TokenType
+  case class Ignore(l: Int, c: Int) extends Token(l, c)
 }
 
-class Token(val tokenType: TokenType, val value: Option[String], val line: Int, val column: Int) {
-  override def toString: String = tokenType + (value match {
-    case Some(v) => "(" + v + ")"
-    case None => ""
-  })
-}
+  case class LexerException(val line: Int, val column: Int) extends Exception
 
-case class LexerException(val line: Int, val column: Int) extends Exception
+  class Lexer(source: String) {
+    private val rules: List[(Regex, (Regex.Match => Token))] = List(
+      ("^;".r,                      m => new Token.Semicolon(line, column)),
+      ("^\\[".r,                    m => new Token.LeftBracket(line, column)),
+      ("^\\]".r,                    m => new Token.RightBracket(line, column)),
+      ("^\\(".r,                    m => new Token.LeftParens(line, column)),
+      ("^\\)".r,                    m => new Token.RightParens(line, column)),
+      ("^\\{".r,                    m => new Token.LeftBrace(line, column)),
+      ("^\\}".r,                    m => new Token.RightBrace(line, column)),
+      ("^,".r,                      m => new Token.Comma(line, column)),
+      ("^:".r,                      m => new Token.Colon(line, column)),
+      ("^\\+".r,                    m => new Token.Plus(line, column)),
+      ("^-".r,                      m => new Token.Minus(line, column)),
+      ("^/".r,                      m => new Token.Slash(line, column)),
+      ("^\\*".r,                    m => new Token.Star(line, column)),
+      ("^=>".r,                     m => new Token.FatArrow(line, column)),
+      ("^==".r,                     m => new Token.EqualsEquals(line, column)),
+      ("^!=".r,                     m => new Token.BangEquals(line, column)),
+      ("^!".r,                      m => new Token.Bang(line, column)),
+      ("^>=".r,                     m => new Token.GreaterEqual(line, column)),
+      ("^<=".r,                     m => new Token.LessEqual(line, column)),
+      ("^>".r,                      m => new Token.Greater(line, column)),
+      ("^<".r,                      m => new Token.Less(line, column)),
+      ("^=".r,                      m => new Token.Equals(line, column)),
+      ("^\\bvar\\b".r,              m => new Token.Var(line, column)),
+      ("^\\blet\\b".r,              m => new Token.Let(line, column)),
+      ("^\\bor\\b".r,               m => new Token.Or(line, column)),
+      ("^\\band\\b".r,              m => new Token.And(line, column)),
+      ("^\\bif\\b".r,               m => new Token.If(line, column)),
+      ("^\\belse\\b".r,             m => new Token.Else(line, column)),
+      ("^\\bwhile\\b".r,            m => new Token.While(line, column)),
+      ("^\\bnil\\b".r,              m => new Token.Nil(line, column)),
+      ("^\\breturn\\b".r,           m => new Token.Return(line, column)),
+      ("^\\bTrue\\b".r,             m => new Token.True(line, column)),
+      ("^\\bFalse\\b".r,            m => new Token.False(line, column)),
+      ("^ ".r,                      m => new Token.Ignore(line, column)),
+      ("^(\\n|\\#.*$)".r,           m => {
+        this.line += 1
+        this.column = 0
+        new Token.Ignore(line, column)
+      }),
+    ("^\".*?\"".r,                m => 
+        new Token.Str(m.toString.substring(1, m.toString.length-1), line, column)),
+      ("^\\d+(\\.\\d+)?(?!\\w)".r,  m => new Token.Number(m.toString, line, column)),
+      ("^\\b(?!\\d)\\w+\\b".r,      m => new Token.Identifier(m.toString, line, column))
+    )
 
-class Lexer(source: String) {
-  private sealed abstract class Rule(val regex: Regex)
-  private case class TokenRule(val r: Regex, val tokenType: TokenType) extends Rule(r)
-  private case class MethodRule(val r: Regex, val func: Regex.Match => Token) extends Rule(r)
+    private var current: Int = 0
+    private var line: Int = 0
+    private var column: Int = 0
 
-  private val rules: List[Rule] = List(
-    TokenRule("^;".r,                      TokenType.SEMICOLON),
-    TokenRule("^\\[".r,                    TokenType.LEFT_BRACKET),
-    TokenRule("^\\]".r,                    TokenType.RIGHT_BRACKET),
-    TokenRule("^\\(".r,                    TokenType.LEFT_PARENS),
-    TokenRule("^\\)".r,                    TokenType.RIGHT_PARENS),
-    TokenRule("^\\{".r,                    TokenType.LEFT_BRACE),
-    TokenRule("^\\}".r,                    TokenType.RIGHT_BRACE),
-    TokenRule("^,".r,                      TokenType.COMMA),
-    TokenRule("^:".r,                      TokenType.COLON),
-    TokenRule("^\\+".r,                    TokenType.PLUS),
-    TokenRule("^-".r,                      TokenType.MINUS),
-    TokenRule("^/".r,                      TokenType.SLASH),
-    TokenRule("^\\*".r,                    TokenType.STAR),
-    TokenRule("^=>".r,                     TokenType.FAT_ARROW),
-    TokenRule("^==".r,                     TokenType.EQUALS_EQUALS),
-    TokenRule("^!=".r,                     TokenType.BANG_EQUALS),
-    TokenRule("^!".r,                      TokenType.BANG),
-    TokenRule("^>=".r,                     TokenType.GREATER_EQUAL),
-    TokenRule("^<=".r,                     TokenType.LESS_EQUAL),
-    TokenRule("^>".r,                      TokenType.GREATER),
-    TokenRule("^<".r,                      TokenType.LESS),
-    TokenRule("^=".r,                      TokenType.EQUALS),
-    TokenRule("^\\bvar\\b".r,              TokenType.VAR),
-    TokenRule("^\\blet\\b".r,              TokenType.LET),
-    TokenRule("^\\bor\\b".r,               TokenType.OR),
-    TokenRule("^\\band\\b".r,              TokenType.AND),
-    TokenRule("^\\bif\\b".r,               TokenType.IF),
-    TokenRule("^\\belse\\b".r,             TokenType.ELSE),
-    TokenRule("^\\bwhile\\b".r,            TokenType.WHILE),
-    TokenRule("^\\bnil\\b".r,              TokenType.NIL),
-    TokenRule("^\\breturn\\b".r,           TokenType.RETURN),
-    TokenRule("^\\bTrue\\b".r,             TokenType.TRUE),
-    TokenRule("^\\bFalse\\b".r,            TokenType.FALSE),
-    MethodRule("^ ".r,                     rule_whitespace),
-    MethodRule("^\\n".r,                   rule_newline),
-    MethodRule("^\\#.*$".r,                rule_newline), // Ignore comments
-    MethodRule("^\".*?\"".r,               rule_string),
-    MethodRule("^\\d+(\\.\\d+)?(?!\\w)".r, rule_number),
-    MethodRule("^\\b(?!\\d)\\w+\\b".r,     rule_identifier)
-  )
+    def tokens: List[Token] = {
+      var result: mutable.ListBuffer[Token] = mutable.ListBuffer()
 
-  private var current: Int = 0
-  private var line: Int = 0
-  private var column: Int = 0
+      while (this.current < this.source.length) {
+        val rule: Option[(Regex, Regex.Match => Token)] = 
+          rules.find(r => r._1.findFirstMatchIn(remainingSource).isDefined)
 
-  private def rule_whitespace(m: Regex.Match) = new Token(TokenType.IGNORE, None, line, column)
-  private def rule_newline(m: Regex.Match) = {
-    this.line += 1
-    this.column = 0
-    new Token(TokenType.IGNORE, None, line, column)
-  }
-  private def rule_string(m: Regex.Match) = new Token(TokenType.STRING, 
-    Some(m.toString.substring(1, m.toString.length-1)),
-    line, column)
-  private def rule_number(m: Regex.Match) = new Token(TokenType.NUMBER, Some(m.toString), line, column)
-  private def rule_identifier(m: Regex.Match) = new Token(TokenType.IDENTIFIER, Some(m.toString), line, column)
+        result += (rule match {
+          case Some((regex, func)) => {
+            // We have asserted that this is Some in the find above
+            val m = regex.findFirstMatchIn(remainingSource).get
 
-  def tokens: List[Token] = {
-    var result: mutable.ListBuffer[Token] = mutable.ListBuffer()
+            current += m.end
+            column += m.end
 
-    while (this.current < this.source.length) {
-      val rule = rules.find(_.regex.findFirstMatchIn(remainingSource).isDefined)
-      
-      result += (rule match {
-        case Some(TokenRule(regex, tokenType)) => new Token(tokenType, None, line, column)
-        case Some(MethodRule(regex, func)) => func(regex.findFirstMatchIn(remainingSource).get)
-        case None => throw new LexerException(line, column)
-      })
+            func(m)
+          }
+          case None => throw new LexerException(line, column)
+        })
+      }
 
-      rule.map(r => {
-        val m = r.regex.findFirstMatchIn(remainingSource).get
-        current += m.end
-        column += m.end
-      }) 
-      
+      result.filter(!_.isInstanceOf[Token.Ignore]).toList
     }
 
-    result.filter(_.tokenType != TokenType.IGNORE).toList
+    def remainingSource = this.source.substring(this.current)
   }
-
-  def remainingSource = this.source.substring(this.current)
-}
 
 
 
